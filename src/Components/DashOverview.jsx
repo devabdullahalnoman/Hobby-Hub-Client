3cf9a311a71d172417bcb12d315e1b5e44c1f7ec
devabdullahalnoman <nomanahnaf@gmail.com>
2025-06-26 19:05:41 +0600

    fixed some dashboard issues

--- src/Components/DashOverview.jsx
+++ src/Components/DashOverview.jsx
@@ -14,7 +14,9 @@ const DashOverview = () => {
   console.log(groupsData);
   return (
     <div className="lg:w-10/12 mx-auto">
-      <h1 className="text-5xl md:text-7xl mt-5 font-extrabold">Welcome, {user.displayName}!</h1>
+      <h1 className="text-5xl md:text-7xl mt-5 font-extrabold">
+        Welcome, {user.displayName}!
+      </h1>
       <div className="lg:flex items-center justify-between my-10 rounded-2xl p-6 space-y-5 bg-base-200">
         <div className="avatar">
           <div className="w-50 rounded-full">
@@ -28,28 +30,32 @@ const DashOverview = () => {
       </div>
       <div className="rounded-2xl p-6 bg-base-200">
         <h1 className="text-5xl mb-5">Dashboard</h1>
-      <div>
-        <div className="flex flex-col lg:flex-row justify-center gap-4">
-          <div className="stats shadow w-full bg-base-300 border border-gray-300">
-          <div className="stat">
-            <div className="stat-title text-xl">Total Groups</div>
-            <div className="stat-value text-center">{groupsData.length}</div>
+        <div>
+          <div className="flex flex-col lg:flex-row justify-center gap-4">
+            <div className="stats shadow w-full bg-base-300 border border-gray-300">
+              <div className="stat">
+                <div className="stat-title text-xl">Total Groups</div>
+                <div className="stat-value text-center">
+                  {groupsData.length}
+                </div>
+              </div>
+            </div>
+            <div className="stats shadow w-full bg-base-300 border border-gray-300">
+              <div className="stat">
+                <div className="stat-title text-xl">My Created Groups</div>
+                <div className="stat-value text-center">{myGroups.length}</div>
+              </div>
+            </div>
+            <div className="stats shadow w-full bg-base-300 border border-gray-300">
+              <div className="stat">
+                <div className="stat-title text-xl">My Joined Groups</div>
+                <div className="stat-value text-center">
+                  {myJoinedGroups.length}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="stats shadow w-full bg-base-300 border border-gray-300">
-          <div className="stat">
-            <div className="stat-title text-xl">My Created Groups</div>
-            <div className="stat-value text-center">{myGroups.length}</div>
-          </div>
-        </div>
-        <div className="stats shadow w-full bg-base-300 border border-gray-300">
-          <div className="stat">
-            <div className="stat-title text-xl">My Joined Groups</div>
-            <div className="stat-value text-center">{myJoinedGroups.length}</div>
-          </div>
-        </div>
-        </div>
-      </div>
       </div>
     </div>
   );
